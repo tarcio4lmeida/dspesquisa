1cf556d856411ce44e73e1a534979c5525a3ec8f
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-09-16 14:43:39 -0300

    Criando navegacao com rotas

---
+++ front-web/src/pages/Records/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+
+
+const Records = () =>(
+    <h1>
+        Quais jogos a galera gosta mais?
+    </h1>          
+);
+export default Records;
